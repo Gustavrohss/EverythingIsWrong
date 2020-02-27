408c07d7fe29d759e564ab15570167dfab176dae
gustav <gustav@gustav.laptop>
2020-02-27 11:14:12 +0100

    Some stuff

--- src/js/welcome/welcomeComponent.js
+++ src/js/welcome/welcomeComponent.js
@@ -1,8 +1,33 @@
 import React from 'react'
 
-const WelcomeComponent = ({}) =>
-(<p>
-    This is the welcome screen! Welcome!
-</p>)
+const WelcomeComponent = ({
+    join: [joinLabel, joinCallback], 
+    host: [hostLabel, hostCallback],
+    highScores: [highScoresLabel, highScoresCallback],
+    about: [aboutLabel, aboutCallback]
+}) =>
+(<div>
+    <p>Welcome to Everything is Wrong!</p>
+    <table>
+        <tbody>
+            <tr>
+                <td><button onClick = {joinCallback}>
+                    {joinLabel}
+                </button></td>
+                <td><button onClick = {hostCallback}>
+                    {hostLabel}
+                </button></td>
+            </tr>
+            <tr>
+                <td><button onClick = {highScoresCallback}>
+                    {highScoresLabel}
+                </button></td>
+                <td><button onClick = {aboutCallback}>
+                    {aboutLabel}
+                </button></td>
+            </tr>
+        </tbody>
+    </table>
+</div>)
 
 export default WelcomeComponent
