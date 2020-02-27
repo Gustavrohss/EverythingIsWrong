38e8be5d4bb852cc6b1a135c0fb0261ab41a3206
gustav <gustav@gustav.laptop>
2020-02-27 11:22:50 +0100

    Join Game Component + Container

---
+++ src/js/joinGame/joinGameComponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// Join Game Component.
+// Shown when a player wants to join a game.
+
+/*
+    TODO
+    Pass custom events to inputs, buttons
+    Styling
+*/
+
+const JoinGameComponent = ({}) =>
+(<div>
+    <input>Link</input>
+    <input>Name</input>
+    <button>Join</button>
+</div>)
+
+export default JoinGameComponent
---
+++ src/js/joinGame/joinGameContainer.js
@@ -0,0 +1,17 @@
+import {connect} from 'react-redux'
+import JoinGameComponent from './joinGameComponent'
+
+const mapStateToProps = (state, ownProps) => ({
+
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+
+})
+
+const JoinGameContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(JoinGameComponent)
+
+export default JoinGameContainer
