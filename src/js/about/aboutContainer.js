518177f3415124231d0171eb4e6802aa314b865b
Maja Tennander <majate@kth.se>
2020-02-27 12:07:14 +0100

    Implemented a router

---
+++ src/js/about/aboutContainer.js
@@ -0,0 +1,17 @@
+import AboutComponent from './aboutComponent'
+import {connect} from 'react-redux'
+
+const mapStateToProps = (state, ownProps) => ({
+
+})
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+
+})
+
+const AboutContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AboutComponent)
+
+export default AboutContainer
