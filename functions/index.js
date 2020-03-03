73367dd67e1c51c92680e71ee298136da7ad3fed
gustav <gustav@gustav.laptop>
2020-03-03 17:37:54 +0100

    Nothing works! :)

--- functions/index.js
+++ functions/index.js
@@ -14,3 +14,32 @@ exports.BIGSCORE = functions.database.ref("/highscores/")
             .push()
             .set({name: val.name.toUpperCase(), score: val.score + 1000})
     })
+
+exports.redditRequest = functions.https.onCall(data => {
+    var imgur_client_id = "Client ID 5ca180817daefb2" //api id here.
+
+    /*------ Test code to see that imgur get image api works. */
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", imgur_client_id);
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    const call_api = (uri) => {
+        var url = "https://api.imgur.com/3/" + uri; 
+        return fetch(url, requestOptions)
+        .then(response => response.json())
+        //.then(data => console.log(data))
+        //.catch(error => console.log('error', error));
+    }
+
+    const subreddit_gallery = (subreddit_name="FoodPorn") => {
+        var uri = "gallery/r/"+subreddit_name+"/top/all"; 
+        return call_api(uri)
+    }
+
+    return subreddit_gallery();
+})
