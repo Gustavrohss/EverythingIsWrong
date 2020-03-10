05f79c2cc0faa3024c6897e97505223f61c252b8
Daniel Trochez <trochezdaniel95@gmail.com>
2020-03-10 13:30:47 +0100

    Add test for joining lobby

--- src/js/joinGame/joinGameComponent.js
+++ src/js/joinGame/joinGameComponent.js
@@ -11,15 +11,21 @@ import React from 'react'
 
 const JoinGameComponent = ({
     home: [homeLabel, homeCallback],
-    lobby: [lobbyLabel, lobbyCallback]
-}) =>
-(<div>
+    lobby: [lobbyLabel, lobbyCallback],
+    joinLobby
+}) => {
+const [text, setText] = React.useState("");
+const [code, setCode] = React.useState("");
+
+return (
+<div>
     <div>
-        Name: <input></input>
-        Link: <input></input>
+        Name: <input onChange = {e => setText(e.target.value)}></input>
+        Link: <input style={{ textTransform: "uppercase"}} onChange = {e => setCode(e.target.value) }></input>
     </div>
+    <button onClick = {() => joinLobby(code.toUpperCase(), text)}>JOIN</button>
     <button onClick = {homeCallback}>{homeLabel}</button>
     <button onClick = {lobbyCallback}>{lobbyLabel}</button>
-</div>)
+</div>)}
 
 export default JoinGameComponent
