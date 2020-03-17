8922d175cc236780bbe40c3295c222559701b1ad
Gustavrohss <gustav.rohss@gmail.com>
2020-03-17 21:55:04 +0100

    Started working on a better game round component.

--- src/js/gameRound/gameRoundComponent.js
+++ src/js/gameRound/gameRoundComponent.js
@@ -1,22 +1,76 @@
 import React from 'react'
 
 const GameRoundComponent = ({
-    results: [resultsLabel, resultsCallback]
-}) =>
-(<div>
-    <p>Here's a prompt!</p>
+    results: [resultsLabel, resultsCallback],
+    roundInfo = {
+        promptString: "Which of these locations is secretly a pie?",
+        options: [
+            [
+                "https://i.ytimg.com/vi/Tdac7EAyL80/maxresdefault.jpg",
+                0.3, false
+            ],
+            [
+                "https://i.ytimg.com/vi/WRsPwaDT8Ao/maxresdefault.jpg",
+                0.4, false
+            ],
+            [
+                "https://i.ytimg.com/vi/UobKColhhtU/maxresdefault.jpg",
+                0.7, true
+            ]
+        ],
+        answers: [  // null before the answers start coming in
+            ["Alice", "Bob"],
+            ["Carla", "Duncan", "Erika"],
+            ["Fandango"]
+        ]
+    }
+    /*
+        Exact shape/specification of roundInfo TBD
+        See default argument for testing example
+    */
+}) => {
+
+// State hook
+// https://reactjs.org/docs/hooks-state.html
+const [showResults, setShowResults] = React.useState(false)
+/*
+
+    Images are a column, not a row, currently.
+    This might be preferable for mobile use.
+    Design work left as a TODO.
+
+    What works right now should give an idea of how to implement further functionality.
+
+*/
+return (<div>
+    <p align = {"center"}>{roundInfo.promptString}</p>
     <div>
-        <p>Here's where the options live!</p>
-        <p>Who's dirtier, the apple pie or the chicken nuggets?</p>
-    </div>
-    <div>
-        <p>This div will only be shown when the answers are locked in or time is out!</p>
-        <p>At that point, we'll also highlight the correct answer!</p>
-        <p>We'll also show what players answered, and how many points they got.</p>
-        <p>High Score and Results component might give some inspiration on how to do that.</p>
-        <p>We might wanna know more about what shape the data takes before "plugging it in" here</p>
+        {roundInfo.options.map(
+            ([image, value, correct], index) => 
+            <figure key = {index}>
+                <img 
+                    src = {image}
+                    width = {"400px"} 
+                    height = {"200px"}
+                    alt = {"Placeholder"}
+                    onClick = {() => setShowResults(true) /* Will likely be some sort of listener for database, passed with roundInfo */}>
+                </img> 
+                {showResults ? 
+                (<figcaption>
+                    {value}
+                    {roundInfo.answers[index].map(
+                        (player, index2) => correct ?
+                        (<p key = {index2}>
+                            <b>{player} + 1</b>
+                        </p>) :
+                        (<p key = {index2}>
+                            {player}
+                        </p>))}
+                </figcaption>) : null}
+            </figure>
+        )}
     </div>
     <button onClick={resultsCallback}>{resultsLabel}</button>
-</div>)
+</div>)}
 
 export default GameRoundComponent
