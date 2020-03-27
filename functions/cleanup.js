832861390b0faa3e4ce4d3badd80d3cf178b99b0
Gustavrohss <gustav.rohss@gmail.com>
2020-03-27 14:50:28 +0100

    For real this time

--- functions/cleanup.js
+++ functions/cleanup.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions')
 
 exports.CLEANUP = functions.database.ref("/lobbies/{lobbyID}/players")
     .onDelete((snapshot, context) => {
-        if (Object.keys(context.params).length < 0) {
+        if (Object.keys(context.params).length < 1) {
             snapshot.ref.parent.remove()
         } 
     })
