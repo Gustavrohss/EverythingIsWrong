2f9db611f5776ced8eed750a5b3e44c5ca389f00
Gustavrohss <gustav.rohss@gmail.com>
2020-03-30 16:49:14 +0200

    Remove old version

--- functions/cleanup.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions')
-
-exports.CLEANUP = functions.database.ref("/lobbies/{lobbyID}/players")
-    .onDelete((snapshot, context) => {
-        if (Object.keys(context.params).length < 1) {
-            snapshot.ref.parent.remove()
-        } 
-    })
