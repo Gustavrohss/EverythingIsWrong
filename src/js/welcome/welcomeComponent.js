d5e42773ab8be54a64952117921027b501a70ca6
Kostas Zioris <zioris@kth.se>
2020-04-27 12:54:32 +0200

    paths fixed

--- src/js/welcome/welcomeComponent.js
+++ src/js/welcome/welcomeComponent.js
@@ -5,7 +5,7 @@ import HostGameButton from '../../stylingComponents/HostGameButton.js'
 import HighScoresButton from '../../stylingComponents/HighScoresButton.js';
 import AboutButton from '../../stylingComponents/AboutButton.js'
 import DivBox from '../../stylingComponents/InitialBoard.js'
-import { NavigationBar } from '/home/kostas/Courses2020/DH2642_Dynamic_Web/EverythingIsWrong/src/stylingComponents/NavigationBar'
+import { NavigationBar } from '../../stylingComponents/NavigationBar'
 // The welcome screen component
 const WelcomeComponent = ({
     host: [hostLabel, hostCallback],
