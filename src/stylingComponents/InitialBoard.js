f68d140c6f883fcb8e71036851be28d3673a6ebb
Kostas Zioris <zioris@kth.se>
2020-04-27 13:15:01 +0200

    some changes in initial board

--- src/stylingComponents/InitialBoard.js
+++ src/stylingComponents/InitialBoard.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+const temp = (size, width = 1440) => `${(size / width) * 100}vw`;
 
 
 const DivBox = styled.div`
@@ -7,16 +8,30 @@ const DivBox = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: flex;
+  width: fixed;
   background: linear-gradient(grey, lightblue);
   /*background-image:  url(https://picsum.photos/200/300/);*/
   background-size: cover;
   background: opacity(0.2);
 
-  padding: 150px;
-  border: 1px solid rgba(1, 0, 0, 0.1);
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
-`
+  * {
+    margin: 5px;
+    padding: 5px;
+    box-sizing: border-box;
+  }
+  :root {
+      font-size: ${temp(24)};
+
+      @media (min-width: 468) {
+        font-size: ${temp(18)};
+      }
+
+      @media (min-width: 1024px) {
+        font-size: ${temp(16)};
+      }
+    }
+`;
+
 
 
 export default DivBox
