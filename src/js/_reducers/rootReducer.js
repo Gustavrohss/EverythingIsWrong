3ad359336c7537f5c06548eafd723540978a4a2a
Maja Tennander <maja.tennander@yahoo.se>
2020-05-06 16:35:04 +0200

    Initial redirect middleware

--- src/js/_reducers/rootReducer.js
+++ src/js/_reducers/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import gameSessionReducer from './gameSessionReducer'
 import loaderReducer from './loaderReducer'
+import redirectReducer from './redirectReducer'
 
 
 const createRootReducer = (history) => combineReducers({
   router: connectRouter(history),
+  redirect: redirectReducer,
   gameSession: gameSessionReducer,
   loader: loaderReducer
 })
