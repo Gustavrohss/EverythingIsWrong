da259edf4525e8e26749bd30c9927da459c482de
Maja Tennander <maja.tennander@yahoo.se>
2020-05-08 21:51:04 +0200

    Fixed buttons on welcome page

--- src/js/containers/welcomeContainer.js
+++ src/js/containers/welcomeContainer.js
@@ -1,19 +1,21 @@
 import {connect} from 'react-redux'
 import WelcomeComponent from '../components/welcomeComponent'
-import {populateNavArray} from '../actions/utilActions'
-import {getLoggedIn, getUsername} from '../selectors/gameSessionSelectors'
-import {resetUserHash} from '../actions/gameSessionActions'
-
+import {populateNavArray, navBackToGame} from '../actions/utilActions'
+import {getLoggedIn, getUsername, getInLobby} from '../selectors/gameSessionSelectors'
+import {resetUserHash, leaveLobby} from '../actions/gameSessionActions'
 
 const mapStateToProps = (state, ownProps) => ({
     loggedIn: getLoggedIn(state),
-    name: getUsername(state)
+    name: getUsername(state),
+    inGame: getInLobby(state)
 })
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   join: populateNavArray(ownProps.join, dispatch),
   host: populateNavArray(ownProps.host, dispatch),
   login: populateNavArray(ownProps.login, dispatch),
+  quit: ["Quit Game", () => dispatch(leaveLobby())],
+  rejoin: ["Rejoin Game", () => dispatch(navBackToGame())],
   signout: () => {
     dispatch(resetUserHash())
     /*dispatch(setUsername(name))
